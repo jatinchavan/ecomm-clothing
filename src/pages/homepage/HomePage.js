8ab476c1dd5ddf9374072aa8e3b2ab966080f9fd
jatinchavan-kibo <62047961+jatinchavan-kibo@users.noreply.github.com>
2021-04-26 00:17:09 +0530

    Create components along with styling

---
+++ src/pages/homepage/HomePage.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+import './homepage.styles.scss';
+import Directory from '../../components/directory/Directory';
+
+//Homepage includes directory
+const HomePage = () => (
+    <div class="homepage">
+        <Directory />
+    </div>
+)
+
+export default HomePage;
