dd2a0523d973f93a0c031a905bff87746264e4a2
jatinchavan-kibo <62047961+jatinchavan-kibo@users.noreply.github.com>
2021-04-28 01:23:46 +0530

    Added shop page, collection preview component and collection preview component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
 import {Route, Switch} from 'react-router-dom';
+import ShopPage from './pages/shop/Shop';
 
 import HomePage from './pages/homepage/HomePage';
 
 function App() {
-  return (
-    <div>
-      <Switch>
-        <Route exact path="/" component={HomePage}/>
-      </Switch>
+    return (
+        <div>
+            <Switch>
+                <Route exact path="/" component={HomePage}/>
+                <Route exact path='/shop' component={ShopPage}/>
+            </Switch>
 
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
