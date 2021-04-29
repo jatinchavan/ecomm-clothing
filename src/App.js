854f1d67de9d956544de9415c8c2a177a923e205
jatinchavan-kibo <62047961+jatinchavan-kibo@users.noreply.github.com>
2021-04-29 18:33:52 +0530

    Integrated firebase for Authentication with SignIn buttons styling

--- src/App.js
+++ src/App.js
@@ -2,20 +2,50 @@ import './App.css';
 import {Route, Switch} from 'react-router-dom';
 import ShopPage from './pages/shop/Shop';
 import Header from './components/header/Header';
+import SignInAndSignUp from './pages/signIn-signUp/SignIn-SignUp';
 
 import HomePage from './pages/homepage/HomePage';
 
-function App() {
-    return (
-        <div>
-            <Header />
-            <Switch>
-                <Route exact path="/" component={HomePage}/>
-                <Route exact path='/shop' component={ShopPage}/>
-            </Switch>
+import {auth} from './firebase/firebase.utils';
+import { Component } from 'react';
 
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentUser : null
+        }
+    }
+
+    unsubscribeFromAuth = null;
+
+    componentDidMount() {
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+            this.setState({
+                currentUser: user
+            });
+        });
+    }
+
+    componentWillUnmount() {
+        this.unsubscribeFromAuth();
+    }
+
+
+    render() {
+        return (
+            <div>
+                <Header currentUser= {this.state.currentUser} />
+                <Switch>
+                    <Route exact path="/" component={HomePage}/>
+                    <Route exact path='/shop' component={ShopPage}/>
+                    <Route exact path='/signin' component={SignInAndSignUp}/>
+                </Switch>
+    
+            </div>
+        );
+    }
 }
 
 export default App;
