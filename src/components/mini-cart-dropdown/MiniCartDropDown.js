6eb2265ce450d9f0bc9c26298827257661a6779d
jatinchavan-kibo <62047961+jatinchavan-kibo@users.noreply.github.com>
2021-05-03 16:49:48 +0530

    New cart-item component that shoes each item in the mini cart drop down

--- src/components/mini-cart-dropdown/MiniCartDropDown.js
+++ src/components/mini-cart-dropdown/MiniCartDropDown.js
@@ -2,12 +2,22 @@ import React from 'react';
 
 import './MiniCartDropDown.styless.scss';
 import CustomButton from '../custom-button/CustomButton';
+import { connect } from 'react-redux';
+import CartItem from '../cart-item/CartItem';
 
-const MiniCartDropDown = () => (
+const MiniCartDropDown = ({ cartItems }) => (
     <div className='cart-dropdown'>
-        <div className='cart-items' />
+        <div className='cart-items' >
+           {
+            cartItems.map(item => <CartItem key={item.id} item={item} />)
+           }
+        </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 )
 
-export default MiniCartDropDown;
+const mapStateToProps = state => ({
+    cartItems : state.cart.cartItems
+});
+
+export default connect(mapStateToProps)(MiniCartDropDown);
